ee8baa1484e36514b6976bdb08ae33da436e424c
Blast06 <lacalculadora06@hotmail.com>
2023-06-15 20:18:52 -0400

    changed name to scanor

--- ios/Flutter/flutter_export_environment 2.sh
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mac/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac/Desktop/PROYECTOS/FLUTTER/Quick Scanner/quick_scanner"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/Desktop/PROYECTOS/FLUTTER/scanor/quick_scanner"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
