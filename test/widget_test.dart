25dd0854d1c59eaacadfe13e2f23c3894f3fe164
Blast06 <lacalculadora06@hotmail.com>
2023-07-20 11:53:57 -0400

    fixed!: error at imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:scanor/main.dart';
 
-import 'package:quick_scanner/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
