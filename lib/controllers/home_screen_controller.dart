a5357adfc3db5b2f6217fcbd857520ed91526330
Blast06 <lacalculadora06@hotmail.com>
2023-07-29 12:04:32 -0400

    chore!: homescreencrt created

---
+++ lib/controllers/home_screen_controller.dart
@@ -0,0 +1,25 @@
+
+
+
+
+
+import 'package:get/get.dart';
+
+import 'package:get/get.dart';
+
+class HomeScreenController extends GetxController {
+
+
+
+@override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+
+@override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+  }
+}
